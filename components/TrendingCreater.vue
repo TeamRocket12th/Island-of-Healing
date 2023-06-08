4aeacbffaa9ba32e9dd51c79207b1e2d7daa6e48
tsaaiiiii <a0909513991@gmail.com>
2023-06-08 23:44:45 +0800

    style: 修改寬度及標題樣式

--- components/TrendingCreater.vue
+++ components/TrendingCreater.vue
@@ -10,10 +10,10 @@ const data = [
 </script>
 
 <template>
-  <div class="container 3xl:pl-[300px] 3xl:pr-[300px]">
-    <h2 class="mb-4">熱門創作者</h2>
+  <div class="container mb-6 3xl:pl-[300px] 3xl:pr-[300px]">
+    <h2 class="py-4 pl-4">熱門創作者</h2>
     <ul class="flex h-[250px] items-center gap-4">
-      <li v-for="(item, index) in data" :key="index" class="w-[200px]">
+      <li v-for="(item, index) in data" :key="index" class="grow">
         <div class="m-auto mb-4 h-[95px] w-[95px] rounded-full bg-[#D9D9D9]"></div>
         <p class="mb-1 text-center">{{ item.name }}</p>
         <p class="mb-4 text-center">心理諮商師</p>
