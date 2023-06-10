9f3565276006cae5d147edc5095d06bba98693e8
dondon <domi61503@gmail.com>
2023-06-10 17:21:00 +0800

    feat: 新增導覽列動畫

--- components/global/NavbarItem.vue
+++ components/global/NavbarItem.vue
@@ -1,15 +1,51 @@
-<script setup lang="ts">
+<script setup>
 const showCategory = ref(false)
 const toggleshowCategory = () => {
   showCategory.value = !showCategory.value
 }
+
+const isHeaderSticky = ref(false)
+const scrollPosition = ref(0)
+
+const handleScroll = () => {
+  const header = document.querySelector('header')
+  header.classList.toggle('sticky', window.scrollY > 0)
+  isHeaderSticky.value = window.scrollY > 0
+  scrollPosition.value = window.scrollY
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
+
+const headerClasses = computed(() => {
+  return {
+    sticky: isHeaderSticky.value,
+    'nav-aos': isHeaderSticky.value
+  }
+})
+const iconClasses = computed(() => {
+  return {
+    sticky: isHeaderSticky.value,
+    'icon-aos': isHeaderSticky.value
+  }
+})
 </script>
 
 <template>
-  <header class="bg-[#CDCDCD]">
+  <header class="fixed top-0 z-50 w-full bg-[#CDCDCD] duration-300 ease-in" :class="headerClasses">
     <div class="container 3xl:pl-[300px] 3xl:pr-[300px]">
-      <nav class="relative flex h-16 items-center justify-between">
-        <h1 class="text-2xl font-bold"><NuxtLink to="/">小島聊癒所</NuxtLink></h1>
+      <nav class="relative flex h-16 items-center justify-between duration-300 ease-in">
+        <h1 class="text-2xl font-bold">
+          <NuxtLink to="/">
+            <div class="text-[32px] duration-100 ease-in" :class="iconClasses">小島聊癒所</div>
+            <!-- <p>Scroll position: {{ scrollPosition }}</p> -->
+          </NuxtLink>
+        </h1>
         <ul class="flex gap-6 text-xl font-medium">
           <li>
             <button @click="toggleshowCategory">
@@ -50,4 +86,13 @@ const toggleshowCategory = () => {
   </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.nav-aos {
+  padding: 25px 0px;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.8);
+}
+.icon-aos {
+  font-size: 24px;
+}
+</style>
