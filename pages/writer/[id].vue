9f25bcedabaff9e1752e0754ee4947d42bb6ba1e
tsaaiiiii <a0909513991@gmail.com>
2023-06-12 10:44:40 +0800

    feat: 新增作者頁線稿切版

--- pages/writer/[id].vue
+++ pages/writer/[id].vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="container flex gap-[27px] pt-[60px] 3xl:pl-[300px] 3xl:pr-[300px]">
     <WriterCard />
     <WriterWorkCard />
   </div>
