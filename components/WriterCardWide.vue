4d63a0cfd488925c9c02fd3db559f2272840548c
woowooyong <woowooyong@gmail.com>
2023-06-12 19:58:58 +0800

    fix: 修正文章頁面layout

--- components/WriterCardWide.vue
+++ components/WriterCardWide.vue
@@ -1,6 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  articleDetail: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
 <template>
-  <div></div>
+  <div class="mb-6 flex items-center justify-between border border-[#CDCDCD] p-6">
+    <div class="flex items-center gap-2">
+      <div class="h-[60px] w-[60px] overflow-hidden rounded-full">
+        <img src="https://picsum.photos/60" alt="writer" />
+      </div>
+      <div>
+        <p>{{ articleDetail.writer }}</p>
+        <p>{{ articleDetail.aboutMe }}</p>
+      </div>
+    </div>
+
+    <button class="flex items-center rounded border px-3 py-1 text-[#9F9F9F]">
+      <Icon name="ic:baseline-plus" size="20" />追蹤
+    </button>
+  </div>
 </template>
 
 <style scoped></style>
--- containers/ArticleSideBar.vue
+++ containers/ArticleSideBar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
+  <div class="col-span-3">
     <h3 class="mb-3 font-bold">文章搜尋</h3>
     <SearchInput class="mb-6" />
     <TrendingList />
