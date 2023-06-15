5605b383a482a5dc40a2d605e3369181ba9ea860
tsaaiiiii <a0909513991@gmail.com>
2023-06-15 16:08:07 +0800

    feat: 更新html結構與新增自我介紹驗證

--- components/account/ProfileForm.vue
+++ components/account/ProfileForm.vue
@@ -87,129 +87,119 @@ const confirmationRule = () => {
               />
             </div>
           </div>
-          <div class="w-full px-6 pt-6">
-            <div class="mb-4 flex gap-4">
-              <div class="w-full">
-                <label for="email" class="mb-2 block">常用信箱</label>
-                <input
-                  id="email"
-                  type="input"
-                  :value="userData.email"
-                  class="w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
-                  readonly="readonly"
-                />
+          <VForm class="w-full px-6 pt-6">
+            <div class="mb-4 w-full gap-4">
+              <label for="email" class="mb-2 block">常用信箱</label>
+              <input
+                id="email"
+                type="input"
+                :value="userData.email"
+                class="w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
+                readonly="readonly"
+              />
+            </div>
+            <div class="mb-4 w-full gap-4">
+              <label for="phone" class="mb-2 block">電話</label>
+              <input
+                id="phone"
+                v-model="userData.phone"
+                type="input"
+                class="w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
+              />
+            </div>
+            <div class="mb-4 w-full gap-4">
+              <label for="userName">修改名稱</label>
+              <VField
+                id="userName"
+                v-model="userData.userName"
+                name="userName"
+                label="*名稱"
+                class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
+                rules="required"
+              />
+              <VErrorMessage name="userName" class="text-red-500" />
+            </div>
+            <div class="mb-4 gap-4">
+              <label for="birthday" class="mb-2 block">生日</label>
+              <div class="w-full cursor-pointer rounded-sm border border-[#D9D9D9]">
+                <VDatePicker v-model="date" expanded>
+                  <template #default="{ togglePopover }">
+                    <div
+                      class="flex cursor-pointer justify-between"
+                      @click="handleDateClick(togglePopover)"
+                    >
+                      <input
+                        id="birthday"
+                        type="text"
+                        class="cursor-pointer py-[7px] pl-3 text-[#7B7B7B] outline-none"
+                        :value="formattedDate"
+                        readonly="readonly"
+                      />
+                      <button class="mr-4">
+                        <Icon name="material-symbols:keyboard-arrow-down-rounded" size="16" />
+                      </button>
+                    </div>
+                  </template>
+                </VDatePicker>
               </div>
             </div>
-            <div class="mb-4 flex gap-4">
-              <div class="w-1/2">
-                <label for="phone" class="mb-2 block">電話</label>
-                <input
-                  id="phone"
-                  v-model="userData.phone"
-                  type="input"
-                  class="w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
-                />
-              </div>
-              <div class="w-1/2">
-                <VForm for="userName"
-                  >修改名稱
-                  <VField
-                    id="userName"
-                    v-model="userData.userName"
-                    name="userName"
-                    label="*名稱"
-                    class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
-                    rules="required"
-                  />
-                  <VErrorMessage name="userName" class="text-red-500" />
-                </VForm>
-              </div>
-            </div>
-            <div class="mb-4 flex gap-4">
-              <div class="w-1/2">
-                <label for="birthday" class="mb-2 block">生日</label>
-                <div class="w-full cursor-pointer rounded-sm border border-[#D9D9D9]">
-                  <VDatePicker v-model="date" expanded>
-                    <template #default="{ togglePopover }">
-                      <div
-                        class="flex cursor-pointer justify-between"
-                        @click="handleDateClick(togglePopover)"
-                      >
-                        <input
-                          id="birthday"
-                          type="text"
-                          class="cursor-pointer py-[7px] pl-3 text-[#7B7B7B] outline-none"
-                          :value="formattedDate"
-                          readonly="readonly"
-                        />
-                        <button class="mr-4">
-                          <Icon name="material-symbols:keyboard-arrow-down-rounded" size="16" />
-                        </button>
-                      </div>
-                    </template>
-                  </VDatePicker>
-                </div>
-              </div>
-              <div class="w-1/2">
-                <VForm for="jobTitle"
-                  >頭銜
-                  <VField
-                    id="jobTitle"
-                    v-model="userData.jobTitle"
-                    name="jobTitle"
-                    label="*頭銜"
-                    class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
-                    rules="required"
-                  />
-                  <VErrorMessage name="jobTitle" class="text-red-500" />
-                </VForm>
-              </div>
+            <div class="mb-4 w-full gap-4">
+              <label for="jobTitle">頭銜</label>
+              <VField
+                id="jobTitle"
+                v-model="userData.jobTitle"
+                name="jobTitle"
+                label="*頭銜"
+                class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
+                rules="required"
+              />
+              <VErrorMessage name="jobTitle" class="text-red-500" />
             </div>
             <div>
-              <VForm for="password"
-                >輸入新密碼
-                <VField
-                  id="password"
-                  v-model="userData.password"
-                  name="password"
-                  label="*密碼"
-                  type="password"
-                  class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
-                  :rules="[passwordRequired, passwordRule]"
-                />
-                <VErrorMessage name="password" class="text-red-500" />
-                <label for="confirmation" class="mt-2 block">再次輸入新密碼</label>
-                <VField
-                  id="confirmation"
-                  v-model="confirmPassword"
-                  name="confirmation"
-                  label="*密碼"
-                  type="password"
-                  class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
-                  :rules="[passwordRequired, confirmationRule]"
-                />
-                <VErrorMessage name="confirmation" class="text-red-500" />
-              </VForm>
+              <label for="password">輸入新密碼</label>
+              <VField
+                id="password"
+                v-model="userData.password"
+                name="password"
+                label="*密碼"
+                type="password"
+                class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
+                :rules="[passwordRequired, passwordRule]"
+              />
+              <VErrorMessage name="password" class="text-red-500" />
+              <label for="confirmation" class="mt-2 block">再次輸入新密碼</label>
+              <VField
+                id="confirmation"
+                v-model="confirmPassword"
+                name="confirmation"
+                label="*密碼"
+                type="password"
+                class="my-2 w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
+                :rules="[passwordRequired, confirmationRule]"
+              />
+              <VErrorMessage name="confirmation" class="text-red-500" />
             </div>
             <div class="mb-9">
               <label for="userIntro" class="mb-2 block">自我介紹</label>
-              <textarea
+              <VField
                 id="userIntro"
                 v-model="userData.userIntro"
-                placeholder="稍微介紹一下自己吧..."
                 name="userIntro"
+                as="textarea"
+                label="*自我介紹"
+                rules="max:10"
                 rows="8"
-                type="text"
-                maxlength="30"
                 class="w-full rounded-sm border border-[#D9D9D9] py-[7px] pl-3 text-[#7B7B7B] outline-none"
-              ></textarea>
+                placeholder="稍微介紹一下自己吧..."
+              />
+              <VErrorMessage name="userIntro" class="text-red-500" />
             </div>
             <div class="mb-16 flex w-full justify-end">
               <button class="rounded-md border bg-[#828282] px-6 py-[7px] text-white">
                 儲存變更
               </button>
             </div>
-          </div>
+          </VForm>
         </div>
       </div>
     </div>
