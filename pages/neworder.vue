24ecac7ebfa0f1c34a1e375826ffd38650289941
tsaaiiiii <a0909513991@gmail.com>
2023-06-16 11:52:37 +0800

    feat: 新增結帳確認與結帳狀態元件線稿切版

--- pages/neworder.vue
+++ pages/neworder.vue
@@ -1,8 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const orderStatus = ref()
+const orderForm = (value: Boolean) => {
+  orderStatus.value = value
+}
+</script>
 
 <template>
   <main>
-    <OrderForm />
+    <OrderForm class="relative" @custom-order="orderForm" />
+    <OrderConfirm v-show="orderStatus" @custom-order="orderForm" />
+    <AlertModal />
   </main>
 </template>
 
