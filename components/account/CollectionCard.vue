051a53601200b136980a0fd21871ef7649366494
tsaaiiiii <a0909513991@gmail.com>
2023-06-26 17:36:33 +0800

    style: 修改格線排版

--- components/account/CollectionCard.vue
+++ components/account/CollectionCard.vue
@@ -9,39 +9,41 @@ defineProps({
 </script>
 
 <template>
-  <div class="col-span-10 col-start-2 grid">
-    <ul v-if="collectedArticles.length > 0">
-      <li v-for="article in collectedArticles" :key="article.id">
-        <div class="flex w-full gap-3 p-3">
-          <div class="w-1/3">
-            <img :src="article.coverUrl" alt="cover" class="w-full" />
-          </div>
-          <div class="w-full">
-            <div class="w-[90%]">
-              <div class="mb-3 flex items-center gap-1">
-                <div class="h-6 w-6 rounded-full bg-primary">
-                  <img src="" alt="" />
+  <div class="grid grid-cols-12">
+    <div class="col-span-10 col-start-2 grid">
+      <ul v-if="collectedArticles.length > 0">
+        <li v-for="article in collectedArticles" :key="article.id">
+          <div class="flex w-full gap-4 p-4">
+            <div class="w-1/3">
+              <img :src="article.coverUrl" alt="cover" class="w-full" />
+            </div>
+            <div class="w-full">
+              <div class="w-[90%]">
+                <div class="mb-3 flex items-center gap-1">
+                  <div class="h-6 w-6 rounded-full bg-primary">
+                    <img src="" alt="" />
+                  </div>
+                  <span class="text-sm font-light">{{ article.role }}</span>
                 </div>
-                <span class="text-sm font-light">{{ article.role }}</span>
+                <h3 class="mb-2 font-serif-tc text-xl font-bold text-primary">
+                  {{ article.articleTitle }}
+                </h3>
+                <p class="mb-2 font-light text-primary-dark">{{ article.articleSummary }}</p>
               </div>
-              <h3 class="mb-2 font-serif-tc text-xl font-bold text-primary">
-                {{ article.articleTitle }}
-              </h3>
-              <p class="mb-2 font-light text-primary-dark">{{ article.articleSummary }}</p>
-            </div>
-            <div class="flex justify-between">
-              <p class="font-light">{{ article.articleDate }}</p>
-              <div class="flex items-center gap-1">
-                <Icon name="mdi:bookmark" size="24" class="text-secondary" />
-                <span class="font-light text-secondary">收藏</span>
+              <div class="flex justify-between">
+                <p class="font-light">{{ article.articleDate }}</p>
+                <div class="flex items-center gap-1">
+                  <Icon name="mdi:bookmark" size="24" class="text-secondary" />
+                  <span class="font-light text-secondary">收藏</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="my-6 w-full border-[0.5px] border-primary"></div>
-      </li>
-    </ul>
-    <span v-else>Loading...</span>
+          <div class="my-6 w-full border-[0.5px] border-primary"></div>
+        </li>
+      </ul>
+      <span v-else>Loading...</span>
+    </div>
   </div>
 </template>
 
--- containers/CollectionList.vue
+++ containers/CollectionList.vue
@@ -6,10 +6,8 @@ if (error.value) {
 }
 </script>
 <template>
-  <div class="grid grid-cols-12 border border-primary bg-sand-100">
-    <h2 class="col-span-12 mb-10 grid pl-10 pt-10 font-serif-tc text-2xl font-bold text-primary">
-      我的收藏
-    </h2>
+  <div class="border border-primary bg-sand-100">
+    <h2 class="mb-10 pl-10 pt-10 font-serif-tc text-2xl font-bold text-primary">我的收藏</h2>
     <ClientOnly>
       <CollectionCard :collected-articles="collectedArticles" />
     </ClientOnly>
