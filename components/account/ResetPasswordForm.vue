e1b1eb57f73e3ab0b5fadfe74c1a68da1f42bf6b
dondon <domi61503@gmail.com>
2023-06-27 11:59:40 +0800

    feat: 新增登入,註冊,寄信,重設密碼精搞切版+欄位驗證

--- components/account/ResetPasswordForm.vue
+++ components/account/ResetPasswordForm.vue
@@ -1,62 +1,121 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const passwordRequired = (value: string) => {
+  if (value && value.trim()) {
+    return true
+  }
+  return '*密碼 為必填'
+}
+const passwordRule = (value: string) => {
+  const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/
+  if (regex.test(value)) {
+    return true
+  }
+  return '*請輸入 8 位以上大小寫英數字組合'
+}
+const password = ref('')
+const passwordCheck = ref('')
+const confirmationRule = () => {
+  if (passwordCheck.value === password.value) {
+    return true
+  }
+  return '*密碼不一致'
+}
+
+// 修改input type & eye icons
+const passwordType = ref('password')
+const pwdEyeOpen = ref(false)
+const pwdEyeClose = ref(true)
+const togglePasswordType = () => {
+  passwordType.value = passwordType.value === 'text' ? 'password' : 'text'
+  pwdEyeOpen.value = !pwdEyeOpen.value
+  pwdEyeClose.value = !pwdEyeClose.value
+}
+const passwordCheckType = ref('password')
+const pwdCheckEyeOpen = ref(false)
+const pwdCheckEyeClose = ref(true)
+const togglePasswordCheckType = () => {
+  passwordCheckType.value = passwordCheckType.value === 'text' ? 'password' : 'text'
+  pwdCheckEyeOpen.value = !pwdCheckEyeOpen.value
+  pwdCheckEyeClose.value = !pwdCheckEyeClose.value
+}
+</script>
 <template>
-  <section class="container flex items-center justify-center py-[100px]">
-    <div class="flex h-[700px] w-full">
-      <div class="w-1/2 bg-[#CDCDCD]"></div>
-      <div class="flex w-1/2 flex-col justify-center border border-[#D9D9D9] px-28 3xl:px-[180px]">
-        <h2 class="py-2 text-[24px] font-bold">重設密碼</h2>
+  <section class="container flex items-center justify-center pb-[55px] font-serif-tc">
+    <div class="flex h-[605px] w-full">
+      <div
+        class="flex w-full flex-col items-center justify-center px-[74px] py-[124px] md:border md:border-primary 3xl:px-[516px] 3xl:py-[132px]"
+      >
+        <div class="w-[288px] text-secondary">
+          <h2 class="mb-4 py-2 text-[24px] font-bold text-primary">重設密碼</h2>
 
-        <form action="" class="w-full">
-          <p class="mt-4 text-[16px]">新密碼 :</p>
+          <VForm action="" class="w-full">
+            <label for="password" class="text-[16px]">新密碼 :</label>
+            <div class="relative mb-1 mt-2">
+              <VField
+                id="password"
+                v-model="password"
+                name="password"
+                :rules="[passwordRequired, passwordRule]"
+                :type="passwordType"
+                class="input-bordered input w-full rounded border-[#BDBDBD] focus:outline-none"
+              />
 
-          <div class="relative mt-2">
-            <input
-              type="password"
-              placeholder="name@example.com"
-              class="input-bordered input w-full rounded border-[#BDBDBD] focus:outline-none"
-            />
-            <!-- <Icon
-              name="ic:outline-remove-red-eye"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-            /> -->
-            <Icon
-              name="material-symbols:visibility-off-outline"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-            />
-          </div>
-          <div class="mt-[10px] flex items-center">
-            <Icon name="mdi:alert-circle-outline" size="15" class="left-3 text-[#6B6B6B]" />
-            <p class="ml-1 text-[14px]">密碼不得少於6個字元</p>
-          </div>
+              <Icon
+                v-if="pwdEyeOpen"
+                name="ic:outline-remove-red-eye"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer"
+                @click="togglePasswordType"
+              />
+              <Icon
+                v-if="pwdEyeClose"
+                name="material-symbols:visibility-off-outline"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer"
+                @click="togglePasswordType"
+              />
+            </div>
+            <VErrorMessage name="password" class="text-[14px] text-red-500" />
+            <div class="mb-4 mt-[10px] flex items-center">
+              <Icon name="mdi:alert-circle-outline" size="15" class="left-3" />
+              <p class="ml-1 text-[14px]">密碼不得少於6個字元</p>
+            </div>
 
-          <p class="mt-6 text-[16px]">重新輸入新密碼 :</p>
-          <div class="relative mt-2">
-            <input
-              type="password"
-              placeholder="name@example.com"
-              class="input-bordered input w-full rounded border-[#BDBDBD] focus:outline-none"
-            />
-            <!-- <Icon
-              name="ic:outline-remove-red-eye"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-            /> -->
-            <Icon
-              name="material-symbols:visibility-off-outline"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-            />
-          </div>
-          <div class="relative mt-4">
-            <button
-              class="btn w-full rounded bg-[#9F9F9F] text-[16px] font-bold text-white hover:bg-slate-600"
-            >
-              儲存變更及登入
-            </button>
-          </div>
-        </form>
+            <label for="passwordCheck" class="mt-6 text-[16px]">重新輸入新密碼 :</label>
+            <div class="relative mb-1 mt-2">
+              <VField
+                id="passwordCheck"
+                v-model="passwordCheck"
+                name="passwordCheck"
+                :rules="[passwordRequired, confirmationRule]"
+                :type="passwordCheckType"
+                class="input-bordered input w-full rounded border-[#BDBDBD] focus:outline-none"
+              />
+              <Icon
+                v-if="pwdCheckEyeOpen"
+                name="ic:outline-remove-red-eye"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer"
+                @click="togglePasswordCheckType"
+              />
+              <Icon
+                v-if="pwdCheckEyeClose"
+                name="material-symbols:visibility-off-outline"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer"
+                @click="togglePasswordCheckType"
+              />
+            </div>
+            <VErrorMessage name="passwordCheck" class="text-[14px] text-red-500" />
+            <div class="relative mt-4">
+              <button
+                class="btn w-full rounded bg-secondary text-[16px] font-bold text-white hover:bg-slate-600"
+              >
+                儲存變更及登入
+              </button>
+            </div>
+          </VForm>
+        </div>
       </div>
     </div>
   </section>
--- components/account/SignUpForm.vue
+++ components/account/SignUpForm.vue
@@ -28,96 +28,186 @@ const handleSignUp = async () => {
     console.log(error.value.data)
   }
 }
+const emailRequired = (value: string) => {
+  if (value && value.trim()) {
+    return true
+  }
+  return '*電子郵件為必填'
+}
+
+const emailRule = (value: string) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  if (regex.test(value)) {
+    return true
+  }
+  return '*請輸入有效的電子郵件'
+}
+
+const passwordRequired = (value: string) => {
+  if (value && value.trim()) {
+    return true
+  }
+  return '*密碼 為必填'
+}
+const passwordRule = (value: string) => {
+  const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/
+  if (regex.test(value)) {
+    return true
+  }
+  return '*請輸入 8 位以上大小寫英數字組合'
+}
+
+const confirmationRule = () => {
+  if (user.confirmpwd === user.password) {
+    return true
+  }
+  return '*密碼不一致'
+}
+// 修改input type & eye icons
+const passwordType = ref('password')
+const pwdEyeOpen = ref(false)
+const pwdEyeClose = ref(true)
+const togglePasswordType = () => {
+  passwordType.value = passwordType.value === 'text' ? 'password' : 'text'
+  pwdEyeOpen.value = !pwdEyeOpen.value
+  pwdEyeClose.value = !pwdEyeClose.value
+}
+const passwordCheckType = ref('password')
+const pwdCheckEyeOpen = ref(false)
+const pwdCheckEyeClose = ref(true)
+const togglePasswordCheckType = () => {
+  passwordCheckType.value = passwordCheckType.value === 'text' ? 'password' : 'text'
+  pwdCheckEyeOpen.value = !pwdCheckEyeOpen.value
+  pwdCheckEyeClose.value = !pwdCheckEyeClose.value
+}
 </script>
 <template>
-  <section class="container flex items-center justify-center py-[100px]">
-    <div class="flex h-[700px] w-full">
-      <div class="w-1/2 bg-[#CDCDCD]"></div>
+  <section class="container flex items-center justify-center pb-[55px] font-serif-tc">
+    <div class="flex h-[605px] w-full">
       <div
-        class="flex w-1/2 flex-col items-center justify-center border border-[#D9D9D9] px-28 3xl:px-[180px]"
+        class="flex w-full flex-col items-center justify-center px-[74px] py-[124px] md:border md:border-primary 3xl:px-[516px] 3xl:py-[132px]"
       >
-        <div class="tabs flex w-full items-center justify-center font-bold">
-          <NuxtLink to="/login" class="tab-bordered tab h-[52px] w-1/2 flex-nowrap py-2 text-[24px]"
-            >登入
-          </NuxtLink>
-          <NuxtLink
-            to="/signup"
-            class="tab-bordered tab tab-active h-[52px] w-1/2 flex-nowrap py-2 text-[24px]"
-            >註冊
-          </NuxtLink>
-        </div>
-        <form class="w-full">
-          <div class="relative mt-4">
-            <Icon
-              name="mdi:email-outline"
-              size="25"
-              class="absolute left-3 top-1/2 translate-y-[-50%] text-[#6B6B6B]"
-            />
-            <input
-              v-model="user.account"
-              type="email"
-              placeholder="信箱"
-              class="input-bordered input w-full rounded border-[#BDBDBD] pl-12 focus:outline-none"
-            />
+        <div class="w-[288px]">
+          <div class="tabs mb-8 hidden w-full items-center justify-center font-bold lg:flex">
+            <NuxtLink
+              to="/login"
+              class="tab-bordered tab h-[52px] w-1/2 flex-nowrap py-2 text-2xl text-[#C1B6A4] hover:text-[#978e7e]"
+              >登入
+            </NuxtLink>
+            <NuxtLink
+              to="/signup"
+              class="tab-bordered tab tab-active h-[52px] w-1/2 flex-nowrap py-2 text-2xl text-primary"
+              >註冊
+            </NuxtLink>
           </div>
+          <div class="mb-6 flex items-center justify-between text-primary lg:mb-8 lg:hidden">
+            <h2 class="text-2xl font-bold">註冊</h2>
+            <NuxtLink
+              to="/login"
+              class="text-[14px] underline decoration-primary underline-offset-2"
+              >立即登入
+            </NuxtLink>
+          </div>
+          <VForm class="w-full text-secondary">
+            <label for="account">帳號</label>
+            <div class="relative mb-1 mt-1">
+              <Icon
+                name="mdi:email-outline"
+                size="25"
+                class="absolute left-3 top-1/2 translate-y-[-50%] text-secondary"
+              />
+              <VField
+                id="account"
+                v-model="user.account"
+                :rules="[emailRequired, emailRule]"
+                type="email"
+                name="email"
+                placeholder="信箱"
+                class="input-bordered input w-full rounded border-[#BDBDBD] pl-12 focus:outline-none"
+              />
+            </div>
+            <VErrorMessage name="email" class="block text-[14px] text-red-500" />
+            <label for="password " class="mt-4 block">密碼</label>
+            <div class="relative mt-1">
+              <Icon
+                name="mdi:lock-outline"
+                size="25"
+                class="absolute left-3 top-1/2 translate-y-[-50%] text-secondary"
+              />
+              <VField
+                id="password"
+                v-model="user.password"
+                :rules="[passwordRequired, passwordRule]"
+                name="password"
+                :type="passwordType"
+                placeholder="密碼"
+                class="input-bordered input mb-1 w-full rounded border-[#BDBDBD] pl-12 focus:outline-none"
+              />
+              <Icon
+                v-if="pwdEyeOpen"
+                name="ic:outline-remove-red-eye"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-secondary"
+                @click="togglePasswordType"
+              />
 
-          <div class="relative mt-4">
-            <Icon
-              name="mdi:lock-outline"
-              size="25"
-              class="absolute left-3 top-1/2 translate-y-[-50%] text-[#6B6B6B]"
-            />
-            <input
-              v-model="user.password"
-              type="password"
-              placeholder="密碼"
-              class="input-bordered input w-full rounded border-[#BDBDBD] pl-12 focus:outline-none"
-            />
-            <!-- <Icon
-              name="ic:outline-remove-red-eye"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-            /> -->
-            <Icon
-              name="material-symbols:visibility-off-outline"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-            />
-          </div>
-          <div class="relative mt-4">
-            <Icon
-              name="mdi:lock-outline"
-              size="25"
-              class="absolute left-3 top-1/2 translate-y-[-50%] text-[#6B6B6B]"
-            />
-            <input
-              v-model="user.confirmpwd"
-              type="password"
-              placeholder="再次輸入密碼"
-              class="input-bordered input w-full rounded border-[#BDBDBD] pl-12 focus:outline-none"
-            />
-            <!-- <Icon
-              name="ic:outline-remove-red-eye"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-              onclick="seePassWord"
-            /> -->
-            <Icon
-              name="material-symbols:visibility-off-outline"
-              size="25"
-              class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-[#6B6B6B]"
-            />
-          </div>
-          <div class="relative mt-4">
-            <button
-              type="button"
-              class="btn w-full rounded bg-[#9F9F9F] text-[16px] font-bold text-white hover:bg-slate-600"
-              @click.prevent="handleSignUp"
-            >
-              註冊
-            </button>
-          </div>
-        </form>
+              <Icon
+                v-if="pwdEyeClose"
+                name="material-symbols:visibility-off-outline"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-secondary"
+                @click="togglePasswordType"
+              />
+            </div>
+            <VErrorMessage name="password" class="block text-[14px] text-red-500" />
+            <label for="checkpassword" class="mt-4 block">再次輸入密碼</label>
+            <div class="relative mt-1">
+              <Icon
+                name="mdi:lock-outline"
+                size="25"
+                class="absolute left-3 top-1/2 translate-y-[-50%] text-secondary"
+              />
+              <VField
+                id="checkpassword"
+                v-model="user.confirmpwd"
+                :rules="[passwordRequired, confirmationRule]"
+                name="passwordCheck"
+                :type="passwordCheckType"
+                placeholder="再次輸入密碼"
+                class="input-bordered input mb-1 w-full rounded border-[#BDBDBD] pl-12 focus:outline-none"
+              />
+              <Icon
+                v-if="pwdCheckEyeOpen"
+                name="ic:outline-remove-red-eye"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-secondary"
+                onclick="seePassWord"
+                @click="togglePasswordCheckType"
+              />
+              <Icon
+                v-if="pwdCheckEyeClose"
+                name="material-symbols:visibility-off-outline"
+                size="25"
+                class="absolute right-3 top-1/2 translate-y-[-50%] cursor-pointer text-secondary"
+                @click="togglePasswordCheckType"
+              />
+            </div>
+            <VErrorMessage name="passwordCheck" class="block text-[14px] text-red-500" />
+            <div class="relative my-4">
+              <button
+                type="button"
+                class="btn w-full rounded bg-secondary text-[16px] font-bold text-white hover:bg-slate-600"
+                @click.prevent="handleSignUp"
+              >
+                註冊
+              </button>
+            </div>
+            <div class="hidden justify-end text-[14px] lg:flex">
+              <p>已成為會員？</p>
+              <NuxtLink to="/login" class="ml-2 underline underline-offset-2">立即登入</NuxtLink>
+            </div>
+          </VForm>
+        </div>
       </div>
     </div>
   </section>
