fad09f59a0c38730224b3322facf49b77ef70619
dondon <domi61503@gmail.com>
2023-06-28 17:34:01 +0800

    style: 新增我的訊息精搞切版

--- pages/account/[id]/messages.vue
+++ pages/account/[id]/messages.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import MessageDetail from '~/components/account/MessageDetail.vue'
+
 definePageMeta({
   layout: 'userlayout',
   requiredAuth: true
 })
 </script>
 <template>
-  <div><MessageCenter /></div>
+  <div>
+    <div class="grid grid-cols-12 py-16 text-primary lg:py-20">
+      <MessageCenter />
+      <MessageDetail />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
