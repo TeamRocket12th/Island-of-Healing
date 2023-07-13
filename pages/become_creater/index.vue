a8bf999b997db28c5cba6ce740c850632543d9d6
tsaaiiiii <a0909513991@gmail.com>
2023-07-14 00:39:59 +0800

    feat: 新增LandingPage申請成為作家頁面

---
+++ pages/become_creater/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'landinglayout'
+})
+
+useHead({ title: '成為作家' })
+</script>
+<template>
+  <div class="bg-sand-100">
+    <LandingIntro />
+    <LandingWriting />
+    <LandingFeatures />
+    <LandingFeedback />
+    <LandingBrand />
+  </div>
+</template>
+
+<style scoped></style>
