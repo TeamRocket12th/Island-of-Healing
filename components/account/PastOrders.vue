14a6e6b314d2e6e154ebe21b8d786bd3a70117dd
tsaaiiiii <a0909513991@gmail.com>
2023-07-18 17:23:57 +0800

    update: 修改歷史訂單編號

--- components/account/PastOrders.vue
+++ components/account/PastOrders.vue
@@ -1,42 +1,25 @@
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig()
-const apiBase = runtimeConfig.public.apiBase
-const userToken = useCookie('token')
 const { formatDate } = useDateFormat()
-
-const OrdersData = ref([])
-const getPastorders = async () => {
-  if (!userToken.value) {
-    return
+defineProps({
+  orderData: {
+    type: Array as () => Article[],
+    required: true,
+    default: () => []
   }
-  try {
-    const res: ApiResponse = await $fetch(`${apiBase}/userorders/get`, {
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${userToken.value}`
-      }
-    })
-    if (res.StatusCode === 200) {
-      OrdersData.value = res.OrdersData
-    }
-  } catch (error: any) {
-    console.log(error)
-  }
-}
-onMounted(getPastorders)
+})
 </script>
 <template>
   <div class="mb-40 border-primary bg-sand-100 px-10 pb-40 pt-10 sm:border">
     <h2 class="mb-6 font-serif-tc text-2xl font-bold text-primary 3xl:mb-10">歷史訂單</h2>
-    <div v-if="OrdersData.length > 0" class="md:hidden">
+    <div v-if="orderData.length > 0" class="md:hidden">
       <ul
-        v-for="(order, index) in OrdersData"
+        v-for="(order, index) in orderData"
         :key="index"
         class="mb-6 flex w-full flex-col rounded border border-secondary bg-white p-3 shadow-[0_2px_20px_2px_rgba(0,0,0,0.07)]"
       >
         <li class="mb-3 text-xl text-primary-dark">{{ formatDate(order.PaidDate) }}</li>
         <li class="mb-2 text-primary-dark">
-          <span class="text-primary">訂單編號 : </span> {{ index + 1 }}
+          <span class="text-primary">訂單編號 : </span> {{ order.MerchantOrderNo }}
         </li>
         <li class="mb-2 text-primary-dark">
           <span class="text-primary">訂單類別 : </span> {{ order.PlanName }}
@@ -51,7 +34,7 @@ onMounted(getPastorders)
       </ul>
     </div>
     <table
-      v-if="OrdersData.length > 0"
+      v-if="orderData.length > 0"
       class="hidden w-full table-fixed border border-slate-300 text-center md:table"
     >
       <thead class="bg-[#EEEAE1]">
@@ -65,19 +48,19 @@ onMounted(getPastorders)
       </thead>
       <tbody>
         <tr
-          v-for="(order, index) in OrdersData"
+          v-for="(order, index) in orderData"
           :key="index"
           class="text-center text-[16px] text-primary-dark"
         >
           <td class="py-3">{{ formatDate(order.PaidDate) }}</td>
-          <td class="py-3">{{ index + 1 }}</td>
+          <td class="py-3">{{ order.MerchantOrderNo }}</td>
           <td class="py-3">{{ order.PlanName }}</td>
           <td class="py-3">${{ order.PlanPrice }}</td>
           <td class="py-3">{{ order.PaymentMethod }}</td>
         </tr>
       </tbody>
     </table>
-    <span v-else> Loaging... </span>
+    <span v-else> Loading... </span>
   </div>
 </template>
 
