37f1ab61249eff905dcbd8c5829032a55cdc87c4
woowooyong <woowooyong@gmail.com>
2023-08-02 00:39:32 +0800

    style: 調整會員設定Layout內容高度

--- layouts/userlayout.vue
+++ layouts/userlayout.vue
@@ -3,7 +3,7 @@
 <template>
   <div id="userpage" class="bg-sand-100">
     <NavbarItem />
-    <div class="flex flex-grow">
+    <div class="flex min-h-[70vh] flex-grow">
       <SettingSidebar class="hidden md:block" />
       <main id="content" class="container relative">
         <slot />
