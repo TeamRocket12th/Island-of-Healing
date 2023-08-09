551694af46249cfd4a2640a9e170022bbddc1286
dondon <domi61503@gmail.com>
2023-08-09 12:47:11 +0800

    feat: TrendingTopics

--- components/topics/TrendingTopics.vue
+++ components/topics/TrendingTopics.vue
@@ -1,6 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const hotTopicList = [
+  {
+    title: '探索情緒的四個迷思',
+    writer: '奧斯丁',
+    date: '2023-05-23  '
+  },
+  {
+    title: '轉化焦慮為生活的優勢',
+    writer: '艾蜜莉',
+    date: '2023-05-23  '
+  },
+  {
+    title: '走出青鳥症候群，活出真實而有意義的生活',
+    writer: '卡夫卡',
+    date: '2023-05-23  '
+  },
+  {
+    title: '行動力的關鍵：三個方法提升自我效能',
+    writer: '莉莉安',
+    date: '2023-05-23  '
+  }
+]
+</script>
 <template>
-  <div>熱門話題</div>
+  <div class="mb-4 text-xl font-bold text-primary">熱門話題</div>
+  <ul>
+    <li
+      v-for="(hotTopic, index) in hotTopicList"
+      :key="index"
+      class="border-b border-primary/10 py-3 text-primary"
+    >
+      <h5 class="inline cursor-pointer border-b border-primary font-medium">
+        {{ hotTopic.title }}
+      </h5>
+      <div class="mt-2 flex font-light">
+        <p>{{ hotTopic.writer }}</p>
+        ·
+        <p>{{ hotTopic.date }}</p>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
--- containers/TopicSidebar.vue
+++ containers/TopicSidebar.vue
@@ -3,8 +3,9 @@ const { searchTopic } = useSearch()
 </script>
 
 <template>
-  <div>
-    <SearchInput :search-fn="searchTopic" />
+  <div class="px-3 py-10">
+    <div class="mb-3 text-xl font-bold text-primary">話題搜尋</div>
+    <SearchInput :search-fn="searchTopic" class="mb-10" />
     <TrendingTopics />
   </div>
 </template>
