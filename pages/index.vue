2c226107f460658ed193d60e8caf47870bd556f8
woowooyong <woowooyong@gmail.com>
2023-12-25 21:14:00 +0800

    update: 修改首頁最新文章Data Fetching

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '~/stores/user'
+
 useSeoMeta({
   title: '小島聊癒所',
   ogDescription: '提供心靈療癒的庇護所',
   ogTitle: '小島聊癒所'
 })
+
+const latestArticles = ref<ArticleCard[]>([])
+const { userData } = storeToRefs(useUserStore())
+
+const { data, error } = await useFetch<ApiResponse>('/api/allarticles', {
+  headers: {
+    'Content-type': 'application/json'
+  },
+  method: 'POST',
+  body: {
+    Page: 1,
+    UserId: userData.value.id || '0'
+  },
+  lazy: true
+})
+
+if (data.value?.StatusCode === 200) {
+  latestArticles.value = data.value?.LatestArticleData
+}
+
+if (error.value) {
+  console.log(error.value)
+}
 </script>
 
 <template>
   <div>
     <BannerArea />
-    <LatestArticleCard />
+    <LatestArticleCard :latest-articles="latestArticles" />
     <TrendingCreater />
     <HomeArticleCard />
   </div>
